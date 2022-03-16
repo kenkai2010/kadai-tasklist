5ccd13880cf4cbda28e4f4c1d3df81670d9d91f4
kaito.hasegawa <kenkai755@gmail.com>
2022-03-16 04:02:54 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
