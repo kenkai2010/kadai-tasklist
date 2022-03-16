2268f871d4f9ca5757bbacd76cb5d5bd6404431b
kaito.hasegawa <kenkai755@gmail.com>
2022-03-16 03:33:07 +0000

    user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_16_005537) do
+ActiveRecord::Schema.define(version: 2022_03_16_024326) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "status"
     t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
