8d0f6af90a085344270d40f754199d530d056076
kaito.hasegawa <kenkai755@gmail.com>
2022-03-18 18:28:49 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     if logged_in?
@@ -17,7 +18,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -51,11 +52,18 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task= Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
   
   # Strong Parameter
   def task_params
     params.require(:task).permit(:status, :content)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
